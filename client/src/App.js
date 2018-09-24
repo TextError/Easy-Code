2b3421740b907dcc6b9b904c5a9cd2266e8ca629
unknown <savinraul1983@yahoo.com>
2018-09-24 16:58:14 +0100

    first route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Components
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+//CSS
 import './style/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
