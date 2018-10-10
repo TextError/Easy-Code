143f753e4331c6f85983ded88904b7729b90d4c1
unknown <savinraul1983@yahoo.com>
2018-10-10 12:00:52 +0100

    didYouKnow

--- client/src/components/HTML/Html.js
+++ client/src/components/HTML/Html.js
@@ -5,7 +5,7 @@ class Html extends Component {
   render() {
     return (
       <div>
-        <DidYouKnow />
+        <DidYouKnow test={(<span>September 9, 1947 <a href='https://en.wikipedia.org/wiki/Grace_Hopper' target='_'>Grace Murray Hopper</a> records 'the first computer bug' in the Harvard Mark II computer's log book: <i>“First actual case of bug being found.”</i></span>)}/>
         html
       </div>
     )
--- client/src/components/HTML/didYouKnow/DidYouKnow.js
+++ client/src/components/HTML/didYouKnow/DidYouKnow.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../../../style/DidYouKnow.css';
 
 class DidYouKnow extends Component {
   render() {
     return (
-      <div>
-        DidYouKnow
-      </div>
+      <div className='did-you-know'>
+        <h1><strong>Did you know: </strong></h1>
+        <div className='did-you-know-box'>
+          <p>{this.props.test}</p>
+        </div>
+    </div>
     )
   }
 }
