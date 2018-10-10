b9ec10f8f563420f9be0771fbd85b8a3c387230d
unknown <savinraul1983@yahoo.com>
2018-10-10 12:09:41 +0100

    react func class didYouKnow

--- client/src/components/HTML/didYouKnow/DidYouKnow.js
+++ client/src/components/HTML/didYouKnow/DidYouKnow.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react'
 import '../../../style/DidYouKnow.css';
 
-class DidYouKnow extends Component {
-  render() {
-    return (
-      <div className='did-you-know'>
-        <h1><strong>Did you know: </strong></h1>
-        <div className='did-you-know-box'>
-          <p>{this.props.test}</p>
-        </div>
+export default (props) => {
+  return (
+    <div className='did-you-know'>
+      <h1><strong>Did you know: </strong></h1>
+      <div className='did-you-know-box'>
+        <p>{props.test}</p>
+      </div>
     </div>
-    )
-  }
-}
+  )
+};
 
-export default DidYouKnow;
