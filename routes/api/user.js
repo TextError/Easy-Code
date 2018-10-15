49e63000c4e0bb4125f427af96099a981fd1d9db
unknown <savinraul1983@yahoo.com>
2018-10-15 18:50:00 +0100

    register user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+//Input Validation
+const validateRegisterInput = require('../../validation/register');
+
+//User Model
+const User = require('../../models/User');
+
 // @route   GET api/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      //Check for email if exists in DB
+      if (user) {
+        errors.email = 'Email allready exists';
+        return res.status(400).json(errors);
+      }
+
+      const newUser  = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+
+    })
+});
 
 module.exports = router;
