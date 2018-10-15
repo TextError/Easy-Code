67bd0565060e48679da7fa6aee46b76e58737513
unknown <savinraul1983@yahoo.com>
2018-10-15 16:48:47 +0100

    backend

--- client/src/components/HTML/Html.js
+++ client/src/components/HTML/Html.js
@@ -16,14 +16,13 @@ class Html extends Component {
   }
 
   onClick() {
-    this.props.randomFacts(randomNumber);
+    this.props.randomFacts();
   }
 
   render() {
     return (
       <div>
         <DidYouKnow test={(<span>September 9, 1947 <a href='https://en.wikipedia.org/wiki/Grace_Hopper' target='_'>Grace Murray Hopper</a> records 'the first computer bug' in the Harvard Mark II computer's log book: <i>“First actual case of bug being found.”</i></span>)}/>
-        html
         <button onClick={this.onClick}>Next</button>
       </div>
     )
--- client/src/react-redux/actions/randomFactsActions.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { GET_RANDOMFACTS } from './types';
-
-export const randomFacts = randomNumber => dispatch => {
-  axios
-}
