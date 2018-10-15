30140dcd4d58c885380ff986038500f39f417ad3
unknown <savinraul1983@yahoo.com>
2018-10-15 18:02:25 +0100

    keys

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./confing/keys').mongoURI;
