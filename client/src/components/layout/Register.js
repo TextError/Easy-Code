d9674caee6548389f69a497a2fd7568761561bba
unknown <savinraul1983@yahoo.com>
2018-10-16 15:23:29 +0100

    input action

--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -1,10 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Register extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      email: '',
+      password: '',
+      password2: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
   render() {
     return (
       <div>
-        register
+        <form onSubmit={this.onSubmit}>
+          <input 
+            type='name'
+            name='userName'
+            value={this.state.userName}
+            placeholder='Name'
+            onChange={this.onChange}
+          />
+          <input 
+            type='email'
+            name='email'
+            value={this.state.email}
+            placeholder='Email'
+            onChange={this.onChange}
+          />
+          <input 
+            type='password'
+            name='password'
+            value={this.state.password}
+            placeholder='Password'
+            onChange={this.onChange}
+          />
+          <input 
+            type='password'
+            name='password2'
+            value={this.state.password2}
+            placeholder='Confirm password'
+            onChange={this.onChange}
+          />
+          <button onClick={this.onClick}>Register</button>
+        </form>
       </div>
     )
   }
